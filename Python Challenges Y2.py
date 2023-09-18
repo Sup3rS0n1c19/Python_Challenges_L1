807b41ca3ac39e1c9b89efbed967e4ddda4e1623
Declan Carroll <98482225+Sup3rS0n1c19@users.noreply.github.com>
2023-09-18 15:04:52 +0100

    Update Python Challenges Y2.py

--- Python Challenges Y2.py
+++ Python Challenges Y2.py
@@ -1,5 +1,22 @@
 #Python Programming Challenges Year 2
 
+#Menu
+def Menu():
+    print("1) Area of a Triangle 2) Power Calculator 3) Addition 4) Age Converter")
+    global userChoice
+    userChoice = int(input("Please select a number between 1 and 7: "))
+    
+    if userChoice == 1:
+        areaTriangle()
+    elif userChoice == 2:
+        powerCalculator()
+    elif userChoice == 3:
+        Addition()
+    elif userChoice == 4:
+        ageConverter()
+
+
+
 #Area of a triangle
 
 def areaTriangle():
@@ -8,7 +25,6 @@ def areaTriangle():
     area = (base * height) / 2
     print("The area of the triangle is " + str(area))
 
-areaTriangle()
 
 #Power Calculator
 
@@ -18,7 +34,6 @@ def powerCalculator():
     circuit_power = (voltage * current)
     print(str(circuit_power) + "W")
 
-powerCalculator()
 
 #Return the Sum of Two Numbers
 
@@ -26,16 +41,26 @@ def Addition():
     num1 = int(input("Please enter the first number: "))
     num2 = int(input("Please enter another number: "))
     total = (num1 + num2)
-    print("The tolal is " + str(total))
+    print("The total is " + str(total))
 
-Addition()
+    if num1%7 or num2%7:
+        division = num1 / num2
+        print("The division answer would be " + str(division))
+
+    elif num1%11 or num2%11:
+        multiplication = num1 * num2
+        print("The multiplication answer would be " + str(multiplication))
+    
 
 #Convert Age to Days
 
 def ageConverter():
-    age = input("Please enter your age: ")
+    age = int(input("Please enter your age: "))
     conversion = (age * 365)
-    print("Your age in days is " + conversion)
+    print("Your age in days is " + str(conversion))
 
-ageConverter()
-    
+    LY_conversion = (age * 365) + (age // 4)
+    print("Your age in days, including leap years, is " + str(LY_conversion))
+
+
+Menu()
